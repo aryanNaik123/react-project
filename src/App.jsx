5bc2a6a1debe76c7c3dc4d5112bf12b13c637b82
Aryan <aryannnaik@outlook.com>
2022-10-25 12:30:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <img src="src\assets\Rectangle 90.png" />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="social">
+      <img src="src\assets\Twitter Icon.png" />
+      <img src="src\assets\Facebook Icon.png" />
+      <img src="src\assets\Instagram Icon.png" />
+      <img src="src\assets\Github Icon.png" />
+    </div>
+  );
+}
